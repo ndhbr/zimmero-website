8d231de54bfaf5820ca0db6500405f7391585926
Andreas Huber <andreas@bavarianbits.de>
2026-05-15 08:38:30 +0200

    Update email, subject, and Navbar button

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,5 @@ export const SITE = {
 };
 
 export const DEMO_URL = 'https://dashboard.zimmero.app';
-export const CONTACT_EMAIL = 'info@bavarianbits.de';
+export const CONTACT_EMAIL = 'info@zimmero.app';
 export const FORM_ENDPOINT = ''; // Later configurable external form endpoint
